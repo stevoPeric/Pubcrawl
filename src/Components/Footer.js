bb6f3b7fddf7b409c807019542bd1115b85c6582
Stevo <stevogk@gmail.com>
2024-04-11 18:58:40 +0200

    pub crawl prototype

---
+++ src/Components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <TwitterIcon />
+          <YouTubeIcon />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Qualtiy</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Carrers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>244-5333-7783</span>
+          <span>77/A</span>
+          <span>Nawala,Colombo</span>
+          <span>foodie@gmail.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './Navbar'
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <Navbar/>
+        <div className='home-banner-container'>
+            <div className='home-bannerImage-container'>
+                <img src={BannerBackground} alt=""/>
+            </div>
+            <div className='home-text-section'>
+                <h1 className='primary-heading'>
+                Embark on the Ultimate Sarajevo Pub Crawl
+                </h1>
+                <p className='primary-text'>
+                Dive into the Heart of Sarajevo's Nightlife Scene: Unleash the City's Vibrant and Unforgettable Nighttime Adventures!
+                </p>
+                <button className='secondary-button'>
+                    Book Now <FiArrowRight/>{""}
+                </button>
+            </div>
+            <div className='home-image-section'>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
